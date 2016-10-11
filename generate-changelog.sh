538faa9ba71494c31b769f823a6fdacfadfaa3f2
ggranum <geoff.granum@gmail.com>
2016-10-11 10:42:43 +0100

    feat(Build): add version argument guard

--- generate-changelog.sh
+++ generate-changelog.sh
@@ -2,19 +2,25 @@
 # npm publish with goodies
 # prerequisites:
 # Create a github access token [https://github.com/settings/tokens] and save it in a file named 'generate-changelog-token.local.txt'
-# Run this script with optional argument `patch`/`minor`/`major`/`<version>`
+# Run this script with required argument `patch`/`minor`/`major`/`<version>`
 # defaults to conventional-recommended-bump
 # and optional argument preset `angular`/ `jquery` ...
 # defaults to conventional-commits-detector
 
+logAndExit () {
+    echo >&2 "$@"
+    exit 1
+}
+
+[ "$#" -ge 1 ] || logAndExit "At least 1 argument required, $# provided"
 
 githubToken=$(cat ./generate-changelog-token.local.txt) &&
   cp package.json _package.json &&
   preset=$(./node_modules/.bin/conventional-commits-detector) &&
   echo 'Using preset: ' $preset &&
-  bump=$(./node_modules/.bin/conventional-recommended-bump -p angular) &&
-  echo 'Bumping ' ${1:-$bump} '. Running npm --no-git-tag-version' &&
-  npm --no-git-tag-version version ${1:-$bump} &>/dev/null &&
+  bump=$1 &&
+  echo 'Bumping ' $bump '. Running npm --no-git-tag-version' &&
+  npm --no-git-tag-version version $bump &>/dev/null &&
   echo 'Updating changelog...' &&
   ./node_modules/.bin/conventional-changelog -i CHANGELOG.md -s -p ${2:-$preset} &&
   echo 'Adding changelog changes to git...' &&
